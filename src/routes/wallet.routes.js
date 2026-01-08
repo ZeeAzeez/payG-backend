ca086eb6326a18b143d6468d96b5589a92f0863e
ZeeAzeez <a.abdulazeez@ardovaplc.com>
2026-01-08 14:49:56 +0100

    updated the fund wallet route

--- src/routes/wallet.routes.js
+++ src/routes/wallet.routes.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middlewares/auth.middleware");
-const { getWalletBalance } = require("../controllers/wallet.controller");
+const {
+  getWalletBalance,
+  fundWallet,
+} = require("../controllers/wallet.controller");
 
 //this collects the wallet balance
 router.get("/balance", authMiddleware, getWalletBalance);
+router.post("/fund", authMiddleware, fundWallet);
 
 module.exports = router;
