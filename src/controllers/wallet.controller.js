026c5dc79840ebbc11f137dee5b1f2f15cd5c655
ZeeAzeez <a.abdulazeez@ardovaplc.com>
2026-01-08 14:36:34 +0100

    included transaction details in the wallet controer

--- src/controllers/wallet.controller.js
+++ src/controllers/wallet.controller.js
@@ -25,6 +25,53 @@ const getWalletBalance = async (req, res) => {
   }
 };
 
+const Transaction = require("../models/Transaction");
+const fundWallet = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { amount, description } = req.body;
+
+    if (!amount || amount <= 0 || !description) {
+      return res.status(400).json({
+        message: "Amount and description are required",
+      });
+    }
+
+    // to find the wallet
+    const wallet = await Wallet.findOne({ user: userId });
+
+    if (!wallet) {
+      return res.status(404).json({
+        message: "Wallet not found",
+      });
+    }
+
+    //to Update the wallet balance
+    wallet.balance += amount;
+    await wallet.save();
+
+    //to create a transaction record
+    await Transaction.create({
+      user: userId,
+      type: "credit",
+      amount,
+      description,
+    });
+
+    //to Respond to the client
+    return res.status(200).json({
+      message: "Wallet funded successfully",
+      balance: wallet.balance,
+    });
+  } catch (error) {
+    console.error("Fund wallet error:", error);
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
 module.exports = {
   getWalletBalance,
+  fundWallet,
 };
