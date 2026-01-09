813b8d92ffed75c31dcb9c89dd3e9431e409ae2f
ZeeAzeez <a.abdulazeez@ardovaplc.com>
2026-01-09 09:48:30 +0100

    initialized paystack payments, created paystack services

---
+++ src/services/paystack.services.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+
+const PAYSTACK_BASE_URL = "https://api.paystack.co";
+
+const initializePayment = async ({ email, amount }) => {
+  const response = await axios.post(
+    `${PAYSTACK_BASE_URL}/transaction/initialize`,
+    {
+      email,
+      amount: amount * 100, // Paystack expects amount in kobo
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  return response.data;
+};
+
+module.exports = {
+  initializePayment,
+};
